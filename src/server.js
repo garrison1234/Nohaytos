fdf85fb61c2148d8c9021069cd5dee795dd0ead8
garrison1234 <handrade88@gmail.com>
2018-07-06 02:08:29 -0700

    fixed server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+var server = require('http').Server(app);
+
+app.use('/assets',express.static(__dirname + '/assets'));
+app.use('/css',express.static(__dirname + '/css'));
+app.use('/js',express.static(__dirname + '/js'));
+app.use('/fonts',express.static(__dirname + '/scss'));
 
 app.get('/',function(req,res){
-  //res.sendFile(__dirname+'/index0.html');
-  //res.sendFile(__dirname+'/game.html');
   res.sendFile(__dirname+'/index.html');
 });
 
