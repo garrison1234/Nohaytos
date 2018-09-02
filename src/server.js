e76d03d8ca328b1aea63857c0950121323fb3215
garrison1234 <handrade88@gmail.com>
2018-09-01 23:57:23 -0700

    started new page

--- src/server.js
+++ src/server.js
@@ -1,47 +1,47 @@
-var express = require('express');
-var path = require('path')
+var express = require("express");
+var path = require("path");
 var app = express();
-var server = require('http').Server(app);
+var server = require("http").Server(app);
 
-app.use('/assets',express.static(__dirname + '/assets'));
-app.use('/css',express.static(__dirname + '/css'));
-app.use('/js',express.static(__dirname + '/js'));
-app.use('/fonts',express.static(__dirname + '/scss'));
+app.use("/assets", express.static(__dirname + "/assets"));
+app.use("/css", express.static(__dirname + "/css"));
+app.use("/js", express.static(__dirname + "/js"));
+app.use("/fonts", express.static(__dirname + "/scss"));
 
-app.get('/',function(req,res){
-  res.sendFile(__dirname+'/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
-app.get('/favicon',function(req,res){
-  res.sendFile(__dirname+'/favicon.ico');
+app.get("/favicon", function(req, res) {
+  res.sendFile(__dirname + "/favicon.ico");
 });
-app.get('/nav', function (req, res) {
-  res.sendFile(__dirname+'/nav.html');
-})
-app.get('/banner', function (req, res) {
-  res.sendFile(__dirname+'/banner.html');
-})
-app.get('/footer', function (req, res) {
-  res.sendFile(__dirname+'/footer.html');
-})
-app.get('/episodios', function (req, res) {
-  res.sendFile(__dirname+'/episodios.html');
-})
-app.get('/acercade', function (req, res) {
-  res.sendFile(__dirname+'/acercade.html');
-})
-app.get('/blog', function (req, res) {
-  res.sendFile(__dirname+'/blog.html');
-})
-app.get('/contacto', function (req, res) {
-  res.sendFile(__dirname+'/contacto.html');
-})
-app.get('/parseTranscriptJSON',function(req,res){
-  res.sendFile(__dirname+'/parseTranscriptJSON.html');
+app.get("/nav", function(req, res) {
+  res.sendFile(__dirname + "/nav.html");
 });
-app.get('/donaeva',function(req,res){
-  res.sendFile(__dirname+'/donaeva.html');
+app.get("/banner", function(req, res) {
+  res.sendFile(__dirname + "/banner.html");
+});
+app.get("/footer", function(req, res) {
+  res.sendFile(__dirname + "/footer.html");
+});
+app.get("/episodios", function(req, res) {
+  res.sendFile(__dirname + "/episodios.html");
+});
+app.get("/acercade", function(req, res) {
+  res.sendFile(__dirname + "/acercade.html");
+});
+app.get("/blog", function(req, res) {
+  res.sendFile(__dirname + "/blog.html");
+});
+app.get("/contacto", function(req, res) {
+  res.sendFile(__dirname + "/contacto.html");
+});
+app.get("/parseTranscriptJSON", function(req, res) {
+  res.sendFile(__dirname + "/parseTranscriptJSON.html");
+});
+app.get("/donaeva", function(req, res) {
+  res.sendFile(__dirname + "/donaeva.html");
 });
 
-server.listen(process.env.PORT || 8081,function(){
-  console.log('Listening on '+server.address().port);
+server.listen(process.env.PORT || 8081, function() {
+  console.log("Listening on " + server.address().port);
 });
